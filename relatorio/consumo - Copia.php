eb24144521edcb9a4c4dc41a4f9dae8b18278469
v1n3k0 <v1n1c1u5_1@hotmail.com>
2016-12-16 08:49:47 -0200

    Correção função consumoDia
    Corrigido problema no formato de data da função consumoDia

--- relatorio/consumo - Copia.php
+++ relatorio/consumo - Copia.php
@@ -1,49 +1,32 @@
 <?php
 	
-	/*Teste*/
 	//include_once('../conexao.php');
-	//echo consumoDia($con, 2150, 2016, 11);
+	//echo consumoDia($con, 2150, 2016, 12);
 	
 	//Retorna string com consumo diario de um mês
 	function consumoDia($con, $id, $ano, $mes){
 
-		if($mes == 1){
-			$numDiaMesAnt = cal_days_in_month(CAL_GREGORIAN, 12, $ano - 1);
-			$auxAno = $ano-1;
-
-			// Converte para data
-			$data = date("Y-m-d",strtotime(str_replace('/','-', $auxAno.'-'.'12'.'-'.$numDiaMesAnt)));
-		}else{
-			$numDiaMesAnt = cal_days_in_month(CAL_GREGORIAN, $mes - 1, $ano);
-			$auxMes= $mes - 1;
-
-			// Converte para data
-			$data = date("Y-m-d",strtotime(str_replace('/','-', $ano.'-'.$auxMes.'-'.$numDiaMesAnt)));		    
-		}
-
-		// Converte a data para modelo do banco de dados
-		$date = date_create($data);
-		$tempo =  date_format($date, 'Y-m-d');
-
-		$result = mysqli_query($con, "SELECT * from unidade WHERE idecoflow = '$id' and tempo like '$tempo%' ORDER by tempo LIMIT 1");
-		$unidadeAnt = mysqli_fetch_object($result);
-
-		//Caso não exista leitura anterior assumi como 0
-		if(isset($unidadeAnt)){
-			$leituraAnt = $unidadeAnt->leitura;
-		}else{
-			$leituraAnt = 0;					
-		}
-
 		//Numero de dias do mes
 		$numDiasMes = cal_days_in_month(CAL_GREGORIAN, $mes, $ano);
-
-		//String para retonar dias e consumo
+		//String com dias e consumo
 		$str = null;
-
 		// Loop para calculo consumo do dias do mes
 		for ($dia = 1; $dia <= $numDiasMes; $dia++){
 			$ant = $dia - 1;
+
+			// Converte a data para modelo do banco de dados
+			$data = date("Y-m-d",strtotime(str_replace('/','-',$ano.'-'.$mes.'-'.$ant)));
+		    $date = date_create($data);
+			$tempo =  date_format($date, 'Y-m-d');
+
+			$result = mysqli_query($con, "SELECT * from unidade WHERE idecoflow = '$id' and tempo like '$tempo%' ORDER by tempo LIMIT 1");
+			$unidadeAnt = mysqli_fetch_object($result);
+			//Caso não exista leitura anterior
+			if(isset($unidadeAnt)){
+				$leituraAnt = $unidadeAnt->leitura;
+			}else{
+				$leituraAnt = 0;					
+			}
 			
 			// Converte a data para modelo do banco de dados 
 			$data = date("Y-m-d",strtotime(str_replace('/','-',$ano.'-'.$mes.'-'.$dia)));
@@ -57,18 +40,13 @@
 			if(isset($unidade)){
 				if($leituraAnt != 0){
 					$unidade->leitura;
-					$consumo = $unidade->leitura - $leituraAnt;
-					$leituraAnt = $unidade->leitura;
+					$consumo = $unidade->leitura - $leituraAnt;	
 				}else{
 					$consumo = 0;
-					$leituraAnt = $unidade->leitura;
 				} 
 			}else{
 				$consumo = 0;
-				$leituraAnt = 0; //possivel correção na logica
 			}
-
-			//Concatena os valores de consumo
 			$str = $str.'['.$dia.','.$consumo.']';
             if($dia != 31) $str = $str.',';
 		}
