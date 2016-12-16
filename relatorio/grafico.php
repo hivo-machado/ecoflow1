6eaeb66eff188582bc732d7df079fd376c174168
v1n3k0 <v1n1c1u5_1@hotmail.com>
2016-12-16 12:06:09 -0200

    Teste javascript

--- relatorio/grafico.php
+++ relatorio/grafico.php
@@ -2,6 +2,8 @@
 <?php include_once("../validar.php"); ?>
 <?php include_once("consumo.php"); ?>
 
+<script src="consumo.js"></script>
+
 <?php 
   // Variaveis da sessão
   $login = $_SESSION['login'];
@@ -9,67 +11,83 @@
 
   //Mes e ano atual
   date_default_timezone_set('UTC');
-  if(isset($_GET['mes']) ){
-    $mes = $_GET['mes'];
-    $ano = date("Y");
-  }else{
-    $mes = date("m");
-    $ano = date("Y");
-  }
+  $mes = date("m");
+  $ano = date("Y");  
 ?>
-
-<!-- API  do google para criação de graficos-->
-<script>
-  google.charts.load('current', {packages: ['corechart', 'line']});
-  google.charts.setOnLoadCallback(drawBasic);
-
-  function drawBasic() {
-
-  var data = new google.visualization.DataTable();
-  data.addColumn('number', 'Dia');
-  data.addColumn('number', 'Consumo');
-
-  data.addRows([
-    <?php 
-      echo consumoDia($con, $login, $ano, $mes);
-    ?>
-  ]);
-
-  var options = {
-    hAxis: {
-      title: 'Dia',
-      //baseline:31,
-      
-      gridlines: {
-        count:15,
-      }
-    },
-    vAxis: {
-      title: 'm³/s'
-    },
-    width: 900,
-    height: 300,
-    title:'Consumo Diário de Água do mês: <?php echo $mes ?>',
-  };  
-
-  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-
-  chart.draw(data, options);
-}
-</script>
 
 <?php 
   //Nome da Unidade
   echo '<h3> Unidade: '.$nome.'</h3>';
  ?>
+<div class="col-md-2">
+  <select class="form-control" name="mes" id="mes" onchange="selecionaMes();">
+    <option></option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>  
+  </select>
+</div>
+
+<script>
+    //API  do google para criação de graficos
+  google.charts.load('current', {packages: ['corechart', 'line']});
+  google.charts.setOnLoadCallback(drawBasic);
+
+  function drawBasic() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'Dia');
+    data.addColumn('number', 'Consumo');
+
+    data.addRows([
+      <?php echo consumoDia($con, $login, $ano, $mes); ?>
+    ]);
+
+    var options = {
+      hAxis: {
+        title: 'Dia',
+        //baseline:31,
+        
+        gridlines: {
+          count:15,
+        }
+      },
+      vAxis: {
+        title: 'm³/s'
+      },
+      width: 900,
+      height: 300,
+      title:'Consumo Diário de Água do mês: <?php echo $mes ?>',
+    };  
+
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+  }
+</script>
 
 <!-- Div do plota grafico -->
 <div id="chart_div"></div>
 
-<?php
-  // Consumo Total do mês 
-  echo '<p>'.consumoMes($con, $login, $ano, $mes, 1).'</p>';
-?>
+<div>
+  <?php
+    // Consumo Total do mês 
+    echo '<p>'.consumoMes($con, $login, $ano, $mes, 1).'</p>';  
+  ?>
+</div>
 
+<p id="demo"></p>
+
+<script>
+  document.getElementById("demo").innerHTML = toCelsius(77);
+</script>
 
 <?php include_once("../footer.php") ?>
