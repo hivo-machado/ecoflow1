68449f4a6ec6deec35d59bba927d9ff4793aefe4
v1n3k0 <v1n1c1u5_1@hotmail.com>
2017-01-16 16:01:22 -0200

    troca UI altera email senha login
    Mudança no modelo de UI

--- login/alteraConta.php
+++ login/alteraConta.php
@@ -7,6 +7,7 @@ include_once("funcoes.php");
 <?php 
 	//variavel de sessão
 	$id = $_SESSION['idecoflow'];
+	$login = $_SESSION['login'];
 
 	//Verificar se existe e-mail cadastrado
 	if(!buscaEmail($con, $id)){
@@ -33,59 +34,70 @@ include_once("funcoes.php");
 	</div>
 </div>
 
+<!--Cabeçalho da form-->
 <div class="row">
-	<div class="col-sm-8 col-sm-offset-2">
-		<form class="form-horizontal" method="POST" action="alterarLogin.php" >				
-			<div class="panel panel-primary">
-				<div class="panel-heading"> <strong> Alterar Login </strong></div>
-				<div class="panel-body">
-						<div class="form-group">
-							<label class="col-sm-4 control-label"> Login</label>
-							<div class="col-sm-8">
-								<input type="text" class="form-control" name="login" placeholder="Novo Login">
-							</div>
-						</div>
-						<div class="form-group">
-				    		<div class="col-sm-4 col-sm-offset-8">
-								<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Atualizar Login</button>
-							</div>
-						</div>
+  <div class="page-header">
+    <h2>Alterar Login</h2>
+  </div>
+</div>
+
+
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<form class="form-horizontal" method="POST" action="alterarLogin.php" >
+			<div class="form-group">
+			    <label class="col-sm-4 control-label">Login Cadastrado</label>
+			    <div class="col-sm-8">
+			      <p class="form-control-static"><?php echo $login ?></p>
+			    </div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-4 control-label">Login</label>
+				<div class="col-sm-8">
+					<input type="text" class="form-control" name="login" placeholder="Novo Login">
+				</div>
+			</div>
+			<div class="form-group">
+	    		<div class="col-sm-4 col-sm-offset-8">
+					<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Atualizar Login</button>
 				</div>
 			</div>
 		</form>
 	</div>
 </div>
 
+<!--Cabeçalho da form-->
 <div class="row">
-	<div class="col-sm-8 col-sm-offset-2">
-		<form class="form-horizontal" method="POST" action="alterarSenha.php" >				
-			<div class="panel panel-primary">
-				<div class="panel-heading"> <strong> Alterar Senha </strong></div>
-				<div class="panel-body">
-						<div class="form-group">
-							<label class="col-sm-4 control-label"> Nova Senha</label>
-							<div class="col-sm-8">
-								<input type="password" class="form-control" name="senha" placeholder="Nova Senha">
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-sm-4 control-label">Confirmar Nova Senha</label>
-							<div class="col-sm-8">
-								<input type="password" class="form-control" name="repetirSenha" placeholder="Repetir Nova Senha">
-							</div>
-						</div>
-						<br>
-						<div class="form-group">
-							<label class="col-sm-4 control-label">Senha</label>
-							<div class="col-sm-8">
-								<input type="password" class="form-control" name="senhaAtual" placeholder="Senha Atual">
-							</div>
-						</div>
-						<div class="form-group">
-				    		<div class="col-sm-4 col-sm-offset-8">
-								<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Atualizar Senha</button>
-							</div>
-						</div>
+  <div class="page-header">
+    <h2>Alterar Senha</h2>
+  </div>
+</div>
+
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<form class="form-horizontal" method="POST" action="alterarSenha.php" >
+			<div class="form-group">
+				<label class="col-sm-4 control-label">Nova Senha</label>
+				<div class="col-sm-8">
+					<input type="password" class="form-control" name="senha" placeholder="Nova Senha">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-4 control-label">Confirmar Nova Senha</label>
+				<div class="col-sm-8">
+					<input type="password" class="form-control" name="repetirSenha" placeholder="Repetir Nova Senha">
+				</div>
+			</div>
+			<br>
+			<div class="form-group">
+				<label class="col-sm-4 control-label">Senha</label>
+				<div class="col-sm-8">
+					<input type="password" class="form-control" name="senhaAtual" placeholder="Senha Atual">
+				</div>
+			</div>
+			<div class="form-group">
+	    		<div class="col-sm-4 col-sm-offset-8">
+					<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span>Atualizar Senha</button>
 				</div>
 			</div>
 		</form>
--- login/alteraEmail.php
+++ login/alteraEmail.php
@@ -28,44 +28,46 @@ include_once("funcoes.php");
 	</div>
 </div>
 
+<!--Cabeçalho da pagina-->
 <div class="row">
-	<div class="col-sm-8 col-sm-offset-2">
-		<form class="form-horizontal" method="POST" action="alterarEmail.php" >				
-			<div class="panel panel-primary">
-				<div class="panel-heading"> <strong> Alterar E-mail </strong></div>
-				<div class="panel-body">
-						<?php if($email = buscaEmail($con, $id)){ ?>
-						<div class="form-group">
-						    <label class="col-sm-4 control-label">E-mail Cadastrado</label>
-						    <div class="col-sm-8">
-						      <p class="form-control-static"><?php echo $email ?></p>
-						    </div>
-						</div>
-						<?php } ?>
-						<div class="form-group">
-							<label class="col-sm-4 control-label">Novo E-mail</label>
-							<div class="col-sm-8">
-								<input type="email" class="form-control" name="email" placeholder="email@email.com">
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="col-sm-4 control-label">Confirmar Novo E-mail</label>
-							<div class="col-sm-8">
-								<input type="email" class="form-control" name="confEmail" placeholder="Repetir e-mail">
-							</div>
-						</div>
-						<br>
-						<div class="form-group">
-							<label class="col-sm-4 control-label">Senha</label>
-							<div class="col-sm-8">
-								<input type="password" class="form-control" name="senha" placeholder="Senha">
-							</div>
-						</div>
-						<div class="form-group">
-				    		<div class="col-sm-4 col-sm-offset-8">
-								<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Atualizar e-mail</button>
-							</div>
-						</div>
+  <div class="page-header">
+    <h2>Alterar E-mail</h2>
+  </div>
+</div>
+
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<form class="form-horizontal" method="POST" action="alterarEmail.php" >
+			<?php if($email = buscaEmail($con, $id)){ ?>
+			<div class="form-group">
+			    <label class="col-sm-4 control-label">E-mail Cadastrado</label>
+			    <div class="col-sm-8">
+			      <p class="form-control-static"><?php echo $email ?></p>
+			    </div>
+			</div>
+			<?php } ?>
+			<div class="form-group">
+				<label class="col-sm-4 control-label">Novo E-mail</label>
+				<div class="col-sm-8">
+					<input type="email" class="form-control" name="email" placeholder="email@email.com">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-4 control-label">Confirmar Novo E-mail</label>
+				<div class="col-sm-8">
+					<input type="email" class="form-control" name="confEmail" placeholder="Repetir e-mail">
+				</div>
+			</div>
+			<br>
+			<div class="form-group">
+				<label class="col-sm-4 control-label">Senha</label>
+				<div class="col-sm-8">
+					<input type="password" class="form-control" name="senha" placeholder="Senha">
+				</div>
+			</div>
+			<div class="form-group">
+	    		<div class="col-sm-4 col-sm-offset-8">
+					<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Atualizar e-mail</button>
 				</div>
 			</div>
 		</form>
