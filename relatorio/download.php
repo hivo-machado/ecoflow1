0550c91b53d7ac7bc3a1096fd0cfe4b9042adb77
v1n3k0 <v1n1c1u5_1@hotmail.com>
2017-03-07 11:00:46 -0300

    Criação de planilha excel
    Uso de biblioteca para criação de tabela excel

---
+++ relatorio/download.php
@@ -0,0 +1,56 @@
+<?php 
+	 include_once("../conexao.php");
+	 include_once("funcaoPlanta.php");
+
+	 //Variavel POST
+	$id = $_POST['id_planta'];
+	$anoInicio = $_POST['anoInicio'];
+	$mesInicio = $_POST['mesInicio'];
+	$diaInicio = $_POST['diaInicio'];
+	$anoFim = $_POST['anoFim'];
+	$mesFim = $_POST['mesFim'];
+	$diaFim = $_POST['diaFim'];
+
+
+	//Consulta o Banco de dados e retorna vetor com nome da unidade e consumo
+	$consumos = consumo($con, $id, $anoInicio, $mesInicio, $diaInicio, $anoFim, $mesFim, $diaFim);
+
+	//Total de consumo da consulta
+	$total = consumoTotal($consumos);
+
+	// Nome do Arquivo do Excel que será gerado
+	$arquivo = 'Planta_'.$id.'_'.$diaInicio.'-'.$mesInicio.'-'.$anoInicio.'_'.$diaFim.'-'.$mesFim.'-'.$anoFim.'.xls';
+
+	// Criamos uma tabela HTML com o formato da planilha para excel
+	$tabela = '<table border="1">';
+	$tabela .= '<tr>';
+	$tabela .= '<td colspan="2"><strong>Tabela de Consumo '.$diaInicio.'/'.$mesInicio.'/'.$anoInicio.' - '.$diaFim.'/'.$mesFim.'/'.$anoFim.'</strong></td>';
+	$tabela .= '</tr>';
+	$tabela .= '<tr>';
+	$tabela .= '<td><strong>Unidade</strong></td>';
+	$tabela .= '<td><strong>Consumo</strong></td>';
+	$tabela .= '</tr>';
+
+	//loop para soma total
+	for($i = 0; $i < count($consumos[0]); $i++){
+		$tabela .= '<tr>';
+		$tabela .= '<td>'.$consumos[0][$i].'</td>';
+		$tabela .= '<td>'.number_format($consumos[1][$i], 3, '', '').'</td>';
+		$tabela .= '</tr>';
+	}
+
+	$tabela .= '<tr>';
+	$tabela .= '<td><strong>Total</strong></td>';
+	$tabela .= '<td><strong>'.number_format($total, 3, '', '').'</strong></td>';
+	$tabela .= '</tr>';
+	$tabela .= '</table>';
+
+	// Força o Download do Arquivo Gerado
+	header ('Cache-Control: no-cache, must-revalidate');
+	header ('Pragma: no-cache');
+	header('Content-Type: application/x-msexcel');
+	header ("Content-Disposition: attachment; filename=\"{$arquivo}\"");
+
+
+	echo $tabela;
+?>
--- relatorio/downloadTabela.php
+++ relatorio/downloadTabela.php
@@ -1,8 +1,12 @@
 <?php 
-	 include_once("../conexao.php");
-	 include_once("funcaoPlanta.php");
+	include_once("../conexao.php");
+	include_once("funcaoPlanta.php");
+	include_once("../php/Classes/PHPExcel.php");
 
-	 //Variavel POST
+	//variavel
+	$cont = 2;
+
+	//Variavel POST
 	$id = $_POST['id_planta'];
 	$anoInicio = $_POST['anoInicio'];
 	$mesInicio = $_POST['mesInicio'];
@@ -10,7 +14,6 @@
 	$anoFim = $_POST['anoFim'];
 	$mesFim = $_POST['mesFim'];
 	$diaFim = $_POST['diaFim'];
-
 
 	//Consulta o Banco de dados e retorna vetor com nome da unidade e consumo
 	$consumos = consumo($con, $id, $anoInicio, $mesInicio, $diaInicio, $anoFim, $mesFim, $diaFim);
@@ -21,36 +24,55 @@
 	// Nome do Arquivo do Excel que será gerado
 	$arquivo = 'Planta_'.$id.'_'.$diaInicio.'-'.$mesInicio.'-'.$anoInicio.'_'.$diaFim.'-'.$mesFim.'-'.$anoFim.'.xls';
 
-	// Criamos uma tabela HTML com o formato da planilha para excel
-	$tabela = '<table border="1">';
-	$tabela .= '<tr>';
-	$tabela .= '<td colspan="2"><strong>Tabela de Consumo '.$diaInicio.'/'.$mesInicio.'/'.$anoInicio.' - '.$diaFim.'/'.$mesFim.'/'.$anoFim.'</strong></td>';
-	$tabela .= '</tr>';
-	$tabela .= '<tr>';
-	$tabela .= '<td><strong>Unidade</strong></td>';
-	$tabela .= '<td><strong>Consumo</strong></td>';
-	$tabela .= '</tr>';
+	// Instanciamos a classe
+	$objPHPExcel = new PHPExcel();
 
-	//loop para soma total
+	// Definimos o estilo da fonte
+	$objPHPExcel->getActiveSheet()->getStyle('A1')->getFont()->setBold(true);
+	$objPHPExcel->getActiveSheet()->getStyle('B1')->getFont()->setBold(true);
+
+	// Criamos as colunas
+	$objPHPExcel->setActiveSheetIndex(0)
+	            ->setCellValue('A1', 'Unidade' )
+	            ->setCellValue('B1', 'Consumo' );
+
+	// Podemos configurar diferentes larguras paras as colunas como padrão
+	$objPHPExcel->getActiveSheet()->getColumnDimension('A')->setAutoSize(true);
+	$objPHPExcel->getActiveSheet()->getColumnDimension('B')->setAutoSize(true);
+	$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setAutoSize(true);
+	$objPHPExcel->getActiveSheet()->getColumnDimension('D')->setAutoSize(true);
+
+	//loop de todas as unidades
 	for($i = 0; $i < count($consumos[0]); $i++){
-		$tabela .= '<tr>';
-		$tabela .= '<td>'.$consumos[0][$i].'</td>';
-		$tabela .= '<td>'.number_format($consumos[1][$i], 3, '', '').'</td>';
-		$tabela .= '</tr>';
+		// Também podemos escolher a posição exata aonde o dado será inserido (coluna, linha, dado);
+		$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0, $cont, $consumos[0][$i]);
+		$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1, $cont, number_format($consumos[1][$i] * 1000, 0, '', '') );
+		$cont++;
 	}
 
-	$tabela .= '<tr>';
-	$tabela .= '<td><strong>Total</strong></td>';
-	$tabela .= '<td><strong>'.number_format($total, 3, '', '').'</strong></td>';
-	$tabela .= '</tr>';
-	$tabela .= '</table>';
+	// Definimos o estilo da fonte
+	$objPHPExcel->getActiveSheet()->getStyle('A'.$cont)->getFont()->setBold(true);
+	$objPHPExcel->getActiveSheet()->getStyle('B'.$cont)->getFont()->setBold(true);
+	
+	// Também podemos escolher a posição exata aonde o dado será inserido (coluna, linha, dado);
+	$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0, $cont, 'TOTAL');
+	$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1, $cont, number_format($total * 1000 , 0, '', '') );
 
-	// Força o Download do Arquivo Gerado
-	header ('Cache-Control: no-cache, must-revalidate');
-	header ('Pragma: no-cache');
-	header('Content-Type: application/x-msexcel');
-	header ("Content-Disposition: attachment; filename=\"{$arquivo}\"");
+	// Podemos renomear o nome das planilha atual, lembrando que um único arquivo pode ter várias planilhas
+	$objPHPExcel->getActiveSheet()->setTitle('Planta-'.$id);
 
+	// Cabeçalho do arquivo para ele baixar
+	header('Content-Type: application/vnd.ms-excel');
+	header("Content-Disposition: attachment;filename=\"{$arquivo}\"");
+	header('Cache-Control: max-age=0');
+	// Se for o IE9, isso talvez seja necessário
+	header('Cache-Control: max-age=1');
 
-	echo $tabela;
-?>
+	// Acessamos o 'Writer' para poder salvar o arquivo
+	$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+
+	// Salva diretamente no output, poderíamos mudar arqui para um nome de arquivo em um diretório ,caso não quisessemos jogar na tela
+	$objWriter->save('php://output'); 
+
+	exit;
+ ?>
