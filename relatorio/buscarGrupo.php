e0e26c3d8d0dd12c4fb5a6b3635c8e338b3b35ca
v1n3k0 <v1n1c1u5_1@hotmail.com>
2017-03-09 09:34:02 -0300

    Consumo grupo
    Correção no link para consumo por grupo

--- relatorio/buscarGrupo.php
+++ relatorio/buscarGrupo.php
@@ -34,7 +34,7 @@
         <a href="listaPlanta.php?id_grupo=<?php echo $grupos->id ?>" class="btn btn-primary btn-xs">
           <span class="glyphicon glyphicon-folder-open" aria-hidden="true"> </span> Abrir
         </a>
-        <a href="listaPlanta.php?id_grupo=<?php echo $grupos->id ?>" class="btn btn-primary btn-xs">
+        <a href="grupoConsumo.php?id_grupo=<?php echo $grupos->id ?>" class="btn btn-primary btn-xs">
           <span class="glyphicon glyphicon-th-list" aria-hidden="true"> </span> Consumo
         </a>
       </td>            
