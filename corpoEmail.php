37b15fa31f23173c882702a75169fc81b2d06f36
v1n3k0 <v1n1c1u5_1@hotmail.com>
2017-04-07 16:48:45 -0300

    Enviar e-mail
    Correção do numero de telefone para contato do email. Corpo de email
    unificado.

---
+++ corpoEmail.php
@@ -0,0 +1,65 @@
+<?php 
+	
+	$headerEmail = "
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>E-mail</title>
+			<style type='text/css' media='screen'>
+				body{
+					background-color: #f6f6f6;
+					color: #1d1d1d;
+					font-family: 'Times New Roman', Times, serif;
+				}
+				h3{
+					margin: 0px 0px 0px 20px;
+					font-family: Arial, Helvetica, sans-serif;
+					color: #00a6d3;
+				}
+				h4{
+					margin: 8px 0px 8px 0px;
+					font-family: Arial, Helvetica, sans-serif;
+					font-size: 16px;
+				}
+				strong{
+					font-family: Arial, Helvetica, sans-serif;
+				}
+				#titulo{
+					padding: 10px;
+				}
+				#conteudo{
+					margin: 15px 30px 15px 40px;
+					font-size: 14px;
+				}
+				#contato{
+					border-top: 1px solid #1d1d1d;
+					margin: 20px 25px 0px 25px;
+					padding-top: 5px;
+					font-size: 13px;
+				}
+			</style>
+		</head>
+		<body>
+			<div id='titulo'>
+				<a href='www.ecoflow.net.br'>
+					<img src='http://www.ecoflow.net.br/images/logonovo.png' alt='Logo Ecoflow' >
+				</a>
+				<h3>Economia de água e gás</h3>
+			</div>
+			<div id='conteudo'>
+	";
+
+	$footerEmail = "
+		</div>
+			<div id='contato'>
+				<h4>Contato Ecoflow</h4>
+				<strong>Tel.:</strong> (12) 3042-0282<br>
+				<strong>Tel.:</strong> (35) 3622-7522<br>
+				<strong>E-mail:</strong> <a href='mailto:contato@ecoflow.net.br'>contato@ecoflow.net.br</a><br>
+				<strong>Site:</strong> <a href='www.ecoflow.net.br'>Ecoflow</a>
+			</div>				
+		</body>
+		</html>
+	";
+
+ ?>
--- login/recuperarSenha.php
+++ login/recuperarSenha.php
@@ -1,5 +1,6 @@
 <?php 
 	include_once('../conexao.php');
+	include_once('../corpoEmail.php');
 
 	$email = $_POST['email'];
 
@@ -13,69 +14,13 @@
 
 			//envia e-email com login e senha
 			$assunto = "Recuperar senha";
-			$menssagem = "
-			<!DOCTYPE html>
-			<html>
-			<head>
-				<title>E-mail</title>
-				<style type='text/css' media='screen'>
-					body{
-						background-color: #f6f6f6;
-						color: #1d1d1d;
-						font-family: 'Times New Roman', Times, serif;
-					}
-					h3{
-						margin: 0px 0px 0px 20px;
-						font-family: Arial, Helvetica, sans-serif;
-						color: #00a6d3;
-					}
-					h4{
-						margin: 8px 0px 8px 0px;
-						font-family: Arial, Helvetica, sans-serif;
-						font-size: 16px;
-					}
-					strong{
-						font-family: Arial, Helvetica, sans-serif;
-					}
-					#titulo{
-						padding: 10px;
-					}
-					#conteudo{
-						margin: 15px 30px 15px 40px;
-						font-size: 14px;
-					}
-					#contato{
-						border-top: 1px solid #1d1d1d;
-						margin: 20px 25px 0px 25px;
-						padding-top: 5px;
-						font-size: 13px;
-					}
-				</style>
-			</head>
-			<body>
-				<div id='titulo'>
-					<a href='www.ecoflow.net.br'>
-						<img src='http://www.ecoflow.net.br/images/logonovo.png' alt='Logo Ecoflow' >
-					</a>
-					<h3>Economia de água e gás</h3>
-				</div>
-				<div id='conteudo'>
-					<h4>Recuperar Senha</h4>
-					Nós recuperamos o login e senha para você.<br>
-					<br>
-					<strong>Login:</strong> $usuario->login<br> 
-					<strong>Senha:</strong> $usuario->senha<br>
-				</div>
-				<div id='contato'>
-					<h4>Contato Ecoflow</h4>
-					<strong>Tel.:</strong> (35) 3622-7522<br>
-					<strong>Cel.:</strong> (35) 98875-8875<br>
-					<strong>E-mail:</strong> <a href='mailto:contato@ecoflow.net.br'>contato@ecoflow.net.br</a><br>
-					<strong>Site:</strong> <a href='www.ecoflow.net.br'>Ecoflow</a>
-				</div>				
-			</body>
-			</html>
-			";
+			$menssagem = $headerEmail."
+				<h4>Recuperar Senha</h4>
+				Nós recuperamos o login e senha para você.<br>
+				<br>
+				<strong>Login:</strong> $usuario->login<br> 
+				<strong>Senha:</strong> $usuario->senha<br>
+			".$footerEmail;
 			$menssagem = wordwrap($menssagem, 70);
 			$headers = "Content-type: text/html; charset=utf-8\r\n";
 			$headers .= "From: <noreplay@ecoflow.net.br>\r\n";
