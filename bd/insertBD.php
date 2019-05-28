949092f477040e5dfdace2fc60d7c1ce8e5e304a
enricofaria <efaria@unifei.edu.br>
2019-05-28 11:16:49 -0300

    Issue#7 e Issue#3

--- bd/insertBD.php
+++ bd/insertBD.php
@@ -8,7 +8,7 @@ $result = mysqli_query($con, "SELECT * FROM xml");
 // loop para os links
 while ( $links =  mysqli_fetch_object($result) ) {
 		// Tempo de execução maxima do programa 120 seg.
-		ini_set('max_execution_time',120);//valor original de 120, alterado para teste 
+		ini_set('max_execution_time',200);//valor original de 120, alterado para teste 
 
 		$xml = simplexml_load_file($links->link);
 	// loop para grupos
@@ -35,46 +35,42 @@ while ( $links =  mysqli_fetch_object($result) ) {
 			}
 			// loop para unidade
 		    foreach ($planta->unidades->unidade as $unidade) {
-
-				$idecoflowUnidade = $unidade->{'id-ecoflow'};
-
-				// verifica se usuario ja existe, senão existir adicionar o novo usuario
-				$resUsuario = mysqli_query($con, "SELECT * FROM usuario WHERE id_unidade = '$idecoflowUnidade'");
-				$objUsuario = mysqli_fetch_object($resUsuario);
-				echo $unidade->{'id-ecoflow'}.'<br>';
-				   if(!isset($objUsuario)){
+				//verifica se o usuario e valido antes de tudo
+				if($unidade->nome != 'null' && $unidade->nome != 'NU' && $unidade->nome != 'N U' && $unidade->nome != 'N-U'){
 					$idecoflowUnidade = $unidade->{'id-ecoflow'};
 					$idecoflowPlanta = $planta->{'id-ecoflow'};
-					$tipo = 'usuario';
-					$status = 'ativo';
-					$sql = "INSERT INTO usuario (id_unidade, id_planta, id_grupo, login, senha, nome, tipo, status) VALUES ('$idecoflowUnidade', '$idecoflowPlanta', '$grupo->id', '$idecoflowUnidade', '$idecoflowUnidade', '$unidade->nome', '$tipo', '$status')";
-					mysqli_query($con, $sql);
-				}
 
-				// Converte a data para modelo do banco de dados
-		    	$data = date("Y-m-d",strtotime(str_replace('/','-',$unidade->timestamp)));
-		    	$date = date_create($data);
-				$tempo =  date_format($date, 'Y-m-d');
+					// verifica se usuario ja existe, senão existir adicionar o novo usuario
+					$resUsuario = mysqli_query($con, "SELECT * FROM usuario WHERE id_unidade = '$idecoflowUnidade'");
+					$objUsuario = mysqli_fetch_object($resUsuario);
+					echo $idecoflowUnidade.'<br>';
+				   	if(!isset($objUsuario)){
+						$tipo = 'usuario';
+						$status = 'ativo';
+						$sql = "INSERT INTO usuario (id_unidade, id_planta, id_grupo, login, senha, nome, tipo, status) VALUES ('$idecoflowUnidade', '$idecoflowPlanta', '$grupo->id', '$idecoflowUnidade', '$idecoflowUnidade', '$unidade->nome', '$tipo', '$status')";
+						mysqli_query($con, $sql);
+					}
 
-				//Converte para hora
-				$hora = substr($unidade->timestamp,-5);
+					// Converte a data para modelo do banco de dados
+					$data = date("Y-m-d",strtotime(str_replace('/','-',$unidade->timestamp)));
+					$date = date_create($data);
+					$tempo =  date_format($date, 'Y-m-d');
 
-				//Altera o "," do XML para "."
-				$leitura = (string)$unidade->leitura;
-				$leitura = str_replace("," , "." , $leitura);
-				$leitura = (float)$leitura;
-				
-				//Verifica o medidor se valido para inserção no banco de dados
-				if($unidade->nome != 'null'){
-			    	$idecoflowUnidade = $unidade->{'id-ecoflow'};
-			    	$idecoflowPlanta = $planta->{'id-ecoflow'};
-				    $sql = "INSERT INTO unidade (idecoflow, tempo, hora, id_planta_fk, nome, medidor, servico, leitura) 
+					//Converte para hora
+					$hora = substr($unidade->timestamp,-5);
+
+					//Altera o "," do XML para "."
+					$leitura = (string)$unidade->leitura;
+					$leitura = str_replace("," , "." , $leitura);
+					$leitura = (float)$leitura;
+
+					$sql = "INSERT INTO unidade (idecoflow, tempo, hora, id_planta_fk, nome, medidor, servico, leitura) 
 				    VALUES ('$idecoflowUnidade', '$tempo', '$hora', '$idecoflowPlanta', '$unidade->nome', '$unidade->medidor', '$unidade->servico', '$leitura')";
-					mysqli_query($con, $sql);
+					mysqli_query($con, $sql);				
+
+				}				
+			}				    
 				}
 		    }
 	    }
-	}
-}
-
 ?>
