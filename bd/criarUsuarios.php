a74a3f8f2ff9e9adaf0aa4187796c87493e33c5a
enricofaria <efaria@unifei.edu.br>
2019-06-14 16:42:54 -0300

    Issue#31

--- bd/criarUsuarios.php
+++ bd/criarUsuarios.php
@@ -21,24 +21,40 @@ while ( $links =  mysqli_fetch_object($result) ) {
 			// loop para unidade
 		    foreach ($planta->unidades->unidade as $unidade) {
 				//verifica se o usuario e valido antes de tudo
-				if($unidade->nome != 'null' && $unidade->nome != 'NU' && $unidade->nome != 'N U' && $unidade->nome != 'N-U'){
+				if($unidade->nome != 'null' && $unidade->nome != 'NU' && $unidade->nome != 'N U' && $unidade->nome != 'N-U' && $unidade->nome != 'T1-RES1' && $unidade->nome != 'T1-RES2' && $unidade->nome != 'T1-RES3' && $unidade->nome != 'T1-RES4' && $unidade->nome != 'T1-RES5' && $unidade->nome != 'T1-RES6' && $unidade->nome != 'T1-RES7' && $unidade->nome != 'T1-RES8'){
 					$idecoflowUnidade = $unidade->{'id-ecoflow'};
-					$idecoflowPlanta = $planta->{'id-ecoflow'};
+					$idecoflowPlanta = $planta->{'id-ecoflow'};	
+					$nome = (string)$unidade->nome;				
+
+					// Converte A e G no mesmo usuario. Criada somente para resolver o problema da instalação do grupo ISF
+					if($grupo->id == 18){						
+						$servicoLetra = $nome[3];
+
+						if($servicoLetra == 'A'){							
+							$nome = str_replace("A" , "" , $nome);
+							$idecoflowGas = $idecoflowUnidade;
+
+						}elseif($servicoLetra == 'G'){							
+							$nome = str_replace("G" , "" , $nome);
+							$idecoflowUnidade = $idecoflowGas;
+						}
+					}
 
 					// verifica se usuario ja existe, senão existir adicionar o novo usuario
 					$resUsuario = mysqli_query($con, "SELECT * FROM usuario WHERE id_unidade = '$idecoflowUnidade'");
 					$objUsuario = mysqli_fetch_object($resUsuario);
 					echo $idecoflowUnidade.'<br>';
-				   	if(!isset($objUsuario)){
+					
+					if(!isset($objUsuario)){
 						$tipo = 'usuario';
 						$status = 'ativo';
-						$sql = "INSERT INTO usuario (id_unidade, id_planta, id_grupo, login, senha, nome, tipo, status) VALUES ('$idecoflowUnidade', '$idecoflowPlanta', '$grupo->id', '$idecoflowUnidade', '$idecoflowUnidade', '$unidade->nome', '$tipo', '$status')";
+						$sql = "INSERT INTO usuario (id_unidade, id_planta, id_grupo, login, senha, nome, tipo, status) 
+						VALUES ('$idecoflowUnidade', '$idecoflowPlanta', '$grupo->id', '$idecoflowUnidade', '$idecoflowUnidade', '$nome', '$tipo', '$status')";
 						mysqli_query($con, $sql);
-					    }			
-
-				    }				
-			    }				    
-			}
+					}			
+				}				
+			}				    
 		}
 	}
+}
 ?>
