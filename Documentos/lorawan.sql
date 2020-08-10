fa9a2dd3e8b93856e577c2395f52a96e80a51f15
enricofaria <efaria@unifei.edu.br>
2020-08-10 15:49:41 -0300

    Nova estrutura do banco e Issue#49

---
+++ Documentos/lorawan.sql
@@ -0,0 +1,41 @@
+CREATE TABLE lorawan_modelos (
+    modelo varchar(50) NOT NULL,
+    fabricante varchar(50) NOT NULL,    
+    quatidade_medidores int(5) NOT NULL,
+    PRIMARY KEY (modelo)
+);
+
+CREATE TABLE lorawan_devices (
+    device_addr varchar(15) NOT NULL,
+    modelo varchar(50),
+    planta int (11) NOT NULL,
+    PRIMARY KEY (device_addr),
+    FOREIGN KEY (modelo) REFERENCES lorawan_modelos(modelo)
+);
+
+CREATE TABLE lorawan_unidades (
+    nome varchar(20) NOT NULL,
+    medidor int(10) NOT NULL,
+    servico tinyint(1) NOT NULL,
+    device_addr varchar(15),
+    PRIMARY KEY (nome),
+    FOREIGN KEY (device_addr) REFERENCES lorawan_devices(device_addr)
+);
+
+CREATE TABLE lorawan_idecoflow (
+    idecoflow varchar(15) NOT NULL,
+    nome varchar(20),
+    PRIMARY KEY (idecoflow),
+    FOREIGN KEY (nome) REFERENCES lorawan_unidades(nome)
+);
+
+CREATE TABLE lorawan_status (
+    device_addr varchar(15),
+    tempo date NOT NULL,
+    hora time NOT NULL,
+    snr int(15) NOT NULL,
+    rssi int(15) NOT NULL,
+    nivel_bateria float NOT NULL,
+    PRIMARY KEY (device_addr, tempo, hora),
+    FOREIGN KEY (device_addr) REFERENCES lorawan_devices(device_addr)    
+);
